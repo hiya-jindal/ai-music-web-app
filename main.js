3a652183b19cc42fb84795ea6c2952cad00dd8ef
hiya-jindal <137200203+hiya-jindal@users.noreply.github.com>
2024-03-25 17:54:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
+song1 = "";
+song2 = "";
+
 function setup() {
-    canvas = createCanvas(380, 380);
+    canvas = createCanvas(600, 500);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide(); 
+}
+
+function preload()
+{
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function draw() {
+    image(video, 0, 0, 380, 380);
 }
